6c32b0149f6ac4be99280dc29bd8ffd7bca6cc8a
Dejan <dejan.curk@gmail.com>
2023-04-11 08:46:17 +0200

    a

--- OOP/Pacient.cs
+++ OOP/Pacient.cs
@@ -8,12 +8,8 @@ namespace OOP
 {
     internal class Pacient
     {
-        string ime;
-        string priimek;
-        int leto_rojstva;
-        double visina;
+        string ime; string priimek; int leto_rojstva; double visina;
         double teza;
-
         public Pacient(string ime, string priimek, int leto_rojstva, double visina, double teza)
         {
             this.leto_rojstva = leto_rojstva;
@@ -22,10 +18,14 @@ namespace OOP
             this.visina = visina;
             this.teza = teza;
         }
-
         public double izracunajITM()
         {
             return teza / Math.Pow(visina,2);
+        }
+
+        public void izpisPacienta()
+        {
+            Console.WriteLine(ime + " " + priimek);
         }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -4,10 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Pacient P1 = new Pacient("Janez", "Novak", 1987, 1.80, 80.0);
-            Pacient P2 = new Pacient("France", "Hrovat", 1987, 1.82, 80.0);
+            Pacient P1 = new("Janez", "Novak", 1987, 1.80, 80.0);
+            Pacient P2 = new("France", "Hrovat", 1987, 1.82, 80.0);
 
             Console.WriteLine(P1.izracunajITM());
+            P1.izpisPacienta();
+            P2.izpisPacienta();
         }
     }
 }
